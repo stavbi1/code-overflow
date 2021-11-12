88b793351e1b63699bed118936e83ede3cf0f911
ofir-e <53109978+ofir-e@users.noreply.github.com>
2021-11-12 12:57:59 +0200

    removed helloworld command

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "code-overflow" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-overflow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-overflow!');
-	});
-
 	let disposableSearchCommand = vscode.commands.registerCommand('code-overflow.search-stackoverflow', async () => {
 		const query = getSelectedText() || await vscode.window.showInputBox();
 		if (!query) { return; }
@@ -31,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const rawResult = await fetchStackExchangeQuery(query);
 		const parsedResult = formatItems(rawResult);
 		
-		// start custom of left side tab (in activity bar)
+		// start of custom left side tab (in activity bar)
 		questionsTreeDataProvider.setData(rawResult);
 		questionsTreeDataProvider.refresh();
 
@@ -43,9 +34,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	});
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableSearchCommand);
-	
+
 	vscode.window.registerTreeDataProvider('questions', questionsTreeDataProvider);
 }
 
