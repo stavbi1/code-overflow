81fd4bb93d0dbcf06a29f5bdcf0338821fef86b7
stavbi1 <stavbi1@gmail.com>
2021-12-18 18:32:00 +0200

    feat(sidebar-provider)

--- src/commands/search/Search.ts
+++ src/commands/search/Search.ts
@@ -1,13 +1,12 @@
-import open = require('open');
+import * as open from 'open';
 import * as vscode from 'vscode';
 
 import StackExchange from '../../stack-exchange/StackExchange';
-import { TreeDataProvider } from "../../vscode/QuestionDataProvider";
 import { Item } from '../../stack-exchange/search/SearchResult';
 import { getSelectedText } from '../../vscode/VsCodeUtil';
 import { Question } from './SearchTypes';
 
-export const search = async (questionsTreeDataProvider: TreeDataProvider): Promise<void> => {
+export const search = async (setResults: (items: Item[]) => void): Promise<void> => {
     const query: string = getSelectedText() || await vscode.window.showInputBox();
 
 	if (query) {
@@ -15,8 +14,7 @@ export const search = async (questionsTreeDataProvider: TreeDataProvider): Promi
         const parsedResult: Question[] = parseItemsToQuestions(rawResult);
         
         // start of custom left side tab (in activity bar)
-        questionsTreeDataProvider.setData(rawResult);
-        questionsTreeDataProvider.refresh();
+        setResults(rawResult);
 
         const selectedQuestion = await vscode.window.showQuickPick(
             parsedResult.map(result => result.question),
@@ -27,10 +25,10 @@ export const search = async (questionsTreeDataProvider: TreeDataProvider): Promi
             await open(getLinkByQuestion(parsedResult, selectedQuestion));
         }
     }
-}
+};
 
 const getLinkByQuestion = (questions: Question[], question: string): string =>
-    questions.find(result => result.question === question).link
+    questions.find(result => result.question === question).link;
 
 const formatItemTitle = (item: Item): string => `
     ${item.is_answered ? '✅' : '🤔'} ${item.score}🔺 ${item.answer_count}❗
